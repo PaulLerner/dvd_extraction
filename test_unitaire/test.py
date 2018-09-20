6313e147e0c718877623d6f231ac9a07fd1e34a8
Benjamin MAURICE <benjamin.maurice@gmail.com>
2018-09-20 12:14:30 +0200

    Add test_unitaire

---
+++ test_unitaire/test.py
@@ -0,0 +1,59 @@
+from xml.dom import minidom
+from tvd.rip import Ripper
+from glob import glob
+import yaml
+
+files = glob('*.xml')
+
+data = open('data.yml', 'r', encoding = 'utf-8', errors = 'ignore')
+data = data.read()
+data = yaml.load(data)
+
+texts = []
+
+for f in files:
+    fi = open(f, 'r', encoding = 'utf-8', errors = 'ignore' )
+    content = fi.read()
+    content = content.replace('&', '')
+    texts.append(content)
+
+verification = True
+
+for t in texts:
+    if verification :
+        current = minidom.parseString(t)
+        ripper = Ripper()
+        tracks = ripper.get_tracks(current)
+        ripper.print_contentTitle(current)
+        tracks = ripper.sort_tracks(tracks)
+
+        input = data.get(ripper.get_contentTitle(current))
+
+        episodes = ripper.get_episodes(tracks, input.get('from'), input.get('to'))
+        episodes = ripper.sort_episodes(episodes)
+
+        audio    = ripper.get_languages(current, input.get('from'), input.get('to'))
+
+        test_episodes = input.get('episodes')
+        test_audio    = input.get('audio')
+
+        i = 0
+        print('video')
+
+        while i<len(test_episodes) and verification:
+            print(str(test_episodes[i]) + '    ' + str(episodes[i].title))
+            if int(test_episodes[i]) != int(episodes[i].title):
+                verification = False
+
+            i += 1
+
+        j = 0
+        print('audio')
+
+        while j<len(test_audio) and verification:
+            print(str(test_audio[j]) + '    ' + str(audio[j].title))
+            if int(test_audio[j]) != int(audio[j].title):
+                    verification = False
+            j += 1
+
+print(verification)
